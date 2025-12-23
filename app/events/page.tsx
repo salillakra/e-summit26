ab95859970038cabf68a2d0cbd90f135afb94e65
Salil Lakra <salillakra.dev@gmail.com>
2025-12-24 02:00:35 +0530

    feat: wrap EventsPage components with LazyMotion for enhanced animations

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -2,15 +2,18 @@ import Navbar from "@/components/Navbar";
 import FooterSection from "@/components/FooterSection";
 import EventsHero from "@/components/EventsHero";
 import EventSchedule from "@/components/EventSchedule";
+import { domAnimation, LazyMotion } from "framer-motion";
 export default function EventsPage() {
   return (
     <div className="isolate bg-black text-white">
-      <Navbar />
-      <main>
-        <EventsHero />
-        <EventSchedule />
-      </main>
-      <FooterSection />
+      <LazyMotion features={domAnimation}>
+        <Navbar />
+        <main>
+          <EventsHero />
+          <EventSchedule />
+        </main>
+        <FooterSection />
+      </LazyMotion>
     </div>
   );
 }
