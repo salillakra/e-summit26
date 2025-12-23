91718a94c7bcb0dce02c58b1cab87a3562c719c1
Salil Lakra <salillakra.dev@gmail.com>
2025-12-24 02:00:30 +0530

    feat: wrap contact page components with LazyMotion for improved animations

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,15 +2,18 @@ import Navbar from "@/components/Navbar";
 import FooterSection from "@/components/FooterSection";
 import ContactHero from "@/components/ContactHero";
 import ContactUs from "@/components/ContactUs";
+import { domAnimation, LazyMotion } from "framer-motion";
 export default function ContactPage() {
   return (
     <div className="isolate bg-black text-white">
-      <Navbar />
-      <main>
-        <ContactHero />
-        <ContactUs />
-      </main>
-      <FooterSection />
+      <LazyMotion features={domAnimation}>
+        <Navbar />
+        <main>
+          <ContactHero />
+          <ContactUs />
+        </main>
+        <FooterSection />
+      </LazyMotion>
     </div>
   );
 }
