a806502f5bfdb8765bd580a2e2ac5e81e2ca608f
Salil Lakra <salillakra.dev@gmail.com>
2025-12-24 02:00:45 +0530

    feat: wrap VenuePage components with LazyMotion for enhanced animations

--- app/venue/page.tsx
+++ app/venue/page.tsx
@@ -2,15 +2,18 @@ import Navbar from "@/components/Navbar";
 import FooterSection from "@/components/FooterSection";
 import VenueHero from "@/components/VenueHero";
 import VenueReveal from "@/components/VenueReveal";
+import { domAnimation, LazyMotion } from "framer-motion";
 export default function VenuePage() {
   return (
     <div className="isolate bg-black text-white">
-      <Navbar />
-      <main>
-        <VenueHero />
-        <VenueReveal />
-      </main>
-      <FooterSection />
+      <LazyMotion features={domAnimation}>
+        <Navbar />
+        <main>
+          <VenueHero />
+          <VenueReveal />
+        </main>
+        <FooterSection />
+      </LazyMotion>
     </div>
   );
 }
