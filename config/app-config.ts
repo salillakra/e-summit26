32fe3f99599fc05b5b6216f6a966e05c5937ae8e
Salil Lakra <salillakra.dev@gmail.com>
2025-12-29 23:14:17 +0530

    feat: update app configuration for ESUMMIT'26 branding and details

--- config/app-config.ts
+++ config/app-config.ts
@@ -3,12 +3,12 @@ import packageJson from "@/package.json";
 const currentYear = new Date().getFullYear();
 
 export const APP_CONFIG = {
-  name: "Studio Admin",
+  name: "ESUMMIT'26",
   version: packageJson.version,
-  copyright: `© ${currentYear}, Studio Admin.`,
+  copyright: `© ${currentYear}, EDC's ESUMMIT'26 Admin.`,
   meta: {
-    title: "Studio Admin - Modern Next.js Dashboard Starter Template",
+    title: "EDC's ESUMMIT'26 Admin Dashboard",
     description:
-      "Studio Admin is a modern, open-source dashboard starter template built with Next.js 16, Tailwind CSS v4, and shadcn/ui. Perfect for SaaS apps, admin panels, and internal tools—fully customizable and production-ready.",
+      "Admin panel for managing E-Summit 2026 organized by the Entrepreneurship Development Cell (EDC) of BIT Mesra.",
   },
 };
