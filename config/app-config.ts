232b918a7ea908f01a6c1850a1c0110bcac48ff1
Salil Lakra <salillakra.dev@gmail.com>
2025-12-30 13:05:49 +0530

    fix: correct application name format in app configuration

--- config/app-config.ts
+++ config/app-config.ts
@@ -3,7 +3,7 @@ import packageJson from "@/package.json";
 const currentYear = new Date().getFullYear();
 
 export const APP_CONFIG = {
-  name: "ESUMMIT'26",
+  name: "E-SUMMIT'26",
   version: packageJson.version,
   copyright: `© ${currentYear}, EDC's ESUMMIT'26 Admin.`,
   meta: {
